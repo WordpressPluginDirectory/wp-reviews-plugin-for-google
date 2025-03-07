322f1b6f28064ed1c5f76aead48fe708aaaa9d96
Justin Gardner <justin@rhynorater.com>
2025-03-07 06:01:58 -0500

    12.6.1

--- wp-reviews-plugin-for-google/include/cache-plugin-filters.php
+++ wp-reviews-plugin-for-google/include/cache-plugin-filters.php
@@ -10,7 +10,9 @@ $list []= 'https://cdn.trustindex.io/loader-feed.js';
 return $list;
 }
 }
+add_filter('rocket_minify_excluded_external_js', 'trustindex_exclude_js');
 add_filter('rocket_exclude_js', 'trustindex_exclude_js');
+add_filter('rocket_delay_js_exclusions', 'trustindex_exclude_js');
 add_filter('litespeed_optimize_js_excludes', 'trustindex_exclude_js');
 add_filter('sgo_javascript_combine_excluded_external_paths', 'trustindex_exclude_js');
 add_filter('sgo_css_combine_exclude', function($list) {
@@ -43,8 +45,11 @@ foreach (array (
  0 => 'instagram',
  1 => 'facebook',
  2 => 'youtube',
- 3 => 'twitter',
- 4 => 'tiktok',
+ 3 => 'google',
+ 4 => 'twitter',
+ 5 => 'tiktok',
+ 6 => 'pinterest',
+ 7 => 'vimeo',
 ) as $platform) {
 $list []= 'trustindex-feed-widget-css-'. $platform;
 }
@@ -57,13 +62,31 @@ return $list;
 });
 add_filter('script_loader_tag', function($tag) {
 if (strpos($tag, 'trustindex') !== false && strpos($tag, '/loader') !== false) {
-$tag = preg_replace('/ (crossorigin|integrity)=[\'"][^\'"]+[\'"]/m', '', $tag);
+$tag = preg_replace('/ (crossorigin|integrity|consent-required|consent-by|consent-id)=[\'"][^\'"]+[\'"]/m', '', $tag);
+$tag = str_replace([
+'consent-original-src-_',
+'consent-original-type-_',
+'type="application/consent"',
+], [
+'src',
+'type',
+'',
+], $tag);
 }
 return $tag;
 }, 9999999);
 add_filter('style_loader_tag', function($tag) {
-if (strpos($tag, 'trustindex') !== false && strpos($tag, '/assets/widget-presetted-css/') !== false) {
-$tag = preg_replace('/ (crossorigin|integrity)=[\'"][^\'"]+[\'"]/m', '', $tag);
+if (strpos($tag, 'trustindex') !== false && (strpos($tag, '/assets/widget-presetted-css/') !== false || strpos($tag, '/ti-preview-box.css') !== false)) {
+$tag = preg_replace('/ (crossorigin|integrity|consent-required|consent-by|consent-id)=[\'"][^\'"]+[\'"]/m', '', $tag);
+$tag = str_replace([
+'consent-original-src-_',
+'consent-original-type-_',
+'type="application/consent"',
+], [
+'src',
+'type',
+'',
+], $tag);
 }
 return $tag;
 }, 9999999);
